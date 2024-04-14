42a14ae4fb99cd025d83c700915ede269a730cc6
Evgeny Evseev <evgeny@evseevs.ru>
2024-04-14 14:18:12 +0300

    prepare for testing

---
+++ README.md
@@ -0,0 +1,12 @@
+# Домашняя работа №6
+## Евсеев Евгений — БПИ212
+
+### Запуск
+
+```bash
+./gradlew jar && ./jazzer --agent_path=libs/jazzer_standalone.jar --cp=libs/jazzer_standalone.jar --cp=build/libs/FuzzingProject-1.0-SNAPSHOT.jar --target_class=ru.hse.CalcFuzzingTarget
+```
+
+## Отчет
+
+[//]: # (### Ошибка 1 )
--- src/main/java/ru/hse/CalcFuzzingTarget.java
+++ src/main/java/ru/hse/CalcFuzzingTarget.java
@@ -4,15 +4,23 @@ import calc.Calc;
 import calc.CalcException;
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
+import java.util.Random;
+
 public class CalcFuzzingTarget {
-    public static void fuzzerTestOneInput(FuzzedDataProvider data){
+    private static final Random random = new Random();
+
+    public static void fuzzerTestOneInput(FuzzedDataProvider data) {
         Calc calc = new Calc();
-        String opnstr = "", s = ?;
+        String opnstr = "", s = data.consumeString(random.nextInt(1024));
         try {
             opnstr = calc.opn(s);
-        }catch(Exception ce){//используем opn как фильтр, оставляем только корректные выражения
+        } catch (Exception ignored) {//используем opn как фильтр, оставляем только корректные выражения
+            return;
         }
-        ?
-        //считаем, отсеивая лишнее
+
+        try {
+            calc.calculate(opnstr);
+        } catch (CalcException ignored) {
+        }
     }
 }
